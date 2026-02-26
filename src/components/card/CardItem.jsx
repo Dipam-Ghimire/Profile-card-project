04d10a59f6bb70e4d69556856dbd29d328715731
Dipam Ghimire <dipamghi123@gmail.com>
2026-02-26 08:20:26 +0545

    Profile-card

--- src/components/card/CardItem.jsx
+++ src/components/card/CardItem.jsx
@@ -1,4 +1,3 @@
-import something from '../../assets/images/images.jpg';
 import classes from './CardItem.module.css';
 
 export default function CardItem(props) {
@@ -6,23 +5,23 @@ export default function CardItem(props) {
   return (
     <div className={classes.card}>
       <div className={classes.header}>
-        <img className={classes.avatar} src={something} alt="profile avatar" />
+        <img className={classes.avatar} src={props.image} alt="profile avatar" />
 
         <div className={classes.userInfo}>
-          <h2 className={classes.name}>John Doe</h2>
-          <p className={classes.role}>Frontend developer</p>
+          <h2 className={classes.name}>{props.name}</h2>
+          <p className={classes.role}>{props.role}</p>
         </div>
       </div>
 
       <div className={classes.details}>
         <div className={classes.detailItem}>
           <span className={classes.label}>Email:</span>
-          <span>john@example.com</span>
+          <span>{props.email}</span>
         </div>
 
         <div className={classes.detailItem}>
           <span className={classes.label}>Location:</span>
-          <span>Kathmandu, Nepal</span>
+          <span>{props.location}</span>
         </div>
 
         <div className={classes.detailItem}>
--- src/components/card/CardList.jsx
+++ src/components/card/CardList.jsx
@@ -1,13 +1,14 @@
 import CardItem from "./CardItem";
 import classes from "./CardList.module.css";
+import something from '../../assets/images/images.jpg';
 
 export default function CardList() {
     return (
         <div className={classes.listContainer}>
-            <CardItem name="John Doe" role="Frontend Developer" />
-            <CardItem name="Jane Smith" role="Backend Developer" />
-            <CardItem name="Alex Johnson" role="UI/UX Designer" />
-            <CardItem name="Sarah Williams" role="Full Stack Developer" />
+            <CardItem name="John Doe" role="Frontend Developer" image={something} email="john@example.com" location="Kathmandu, Nepal"/>
+            <CardItem name="Jane Smith" role="Backend Developer" image={something} email="jane@example.com" location="Pokhara, Nepal"/>
+            <CardItem name="Alex Johnson" role="UI/UX Designer" image={something} email="alex@example.com" location="Biratnagar, Nepal"/>
+            <CardItem name="Sarah Williams" role="Full Stack Developer" image={something} email="sarah@example.com" location="Lalitpur, Nepal"/>
         </div>
     );
 }
